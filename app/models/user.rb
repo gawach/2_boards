fe7b8ce04235266882020c601550b9280494e449
gawach <gawa9.1998@gmail.com>
2020-04-28 02:05:24 +0900

    modify user.rb etc...

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,14 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validation :name,
-  uniquness: true,
-  length: { maxmum: 16 },
-  format: {
-    width: /\A[a-z0-9]+\z/,
-    message: 'は小文字英数字で入力してください'
-  }
-  validation :password,
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 16 },
+    format: {
+      with: /\A[a-z0-9]+\z/,
+      message: 'は小文字英数字で入力してください'
+    }
+  validates :password,
     length: { minimum: 8 }
 end
